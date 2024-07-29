d759f027ebaa505d264791f7c463332d36ceddec
preetimagadum <preetimagadum2@gmail.com>
2024-07-29 13:00:04 +0530

    test

---
+++ assignment1/map.js
@@ -0,0 +1,16 @@
+let enames=["swati","preeti"]
+/*let new_names=[]
+
+let names=enames.map((ename)=>{
+    return ename
+})
+
+console.log(names)*/
+
+/*enames.forEach(myFunction)
+function myFunction(item){
+    console.log(item)
+}*/
+
+let names=enames.slice()
+console.log(names)
