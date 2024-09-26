aa629be94d5fce76224bb8b68a3fe7ea658778e7
preetimagadum <preetimagadum2@gmail.com>
2024-09-26 21:51:47 +0530

    test

---
+++ JS Mini Project/Counter/index.js
@@ -0,0 +1,27 @@
+const minus_btn=document.getElementById("minus-btn")
+const count=document.querySelector(".count")
+const changeby=document.getElementById("changeBy")
+
+minus_btn.addEventListener("click",()=>{
+    //alert("123")
+    console.log("minus btn clicked")
+    const countValue=parseInt(count.innerHTML)
+    console.log(countValue)
+    const changebyValue=parseInt(changeby.value)
+    console.log(changebyValue)
+    count.innerText = countValue - changebyValue
+
+})
+
+function plus_btn(){
+    console.log("plus bth clicked")
+    const countValue=parseInt(count.innerHTML)
+    console.log(countValue)
+    const changebyValue=parseInt(changeby.value)
+    console.log(changebyValue)
+    count.innerText = countValue + changebyValue
+}
+
+function resetbtn(){
+    count.innerHTML=0
+}
